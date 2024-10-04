1988e2b0277d33d1b85c981c0ce223bbd1b7a518
an1by <mrkillermafia@gmail.com>
2024-10-04 17:39:25 +0300

    gin start

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"https://github.com/google/uuid"
+	"log"
+	// "fmt"
+	"os"
+	// "github.com/golang-jwt/jwt/v5"
+	// "github.com/google/uuid"
+	"aniby/medods/rest"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env file not found!")
+	}
+}
+
 func main() {
+	// Config
+	serverAddress, exists := os.LookupEnv("SERVER_ADDRESS")
+	if !exists {
+		log.Fatal("SERVER_ADDRESS not found!")
+	}
+
+	// Server
 	router := gin.Default()
+	router.GET("/", rest.GetTokens)
+	router.POST("/", rest.PatchTokens)
+	router.Run(serverAddress)
 }
---
+++ rest/controller.go
@@ -0,0 +1,11 @@
+package rest
+
+import "github.com/gin-gonic/gin"
+
+func GetTokens(c *gin.Context) {
+
+}
+
+func PatchTokens(c *gin.Context) {
+	
+}
